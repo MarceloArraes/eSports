4cb23e8fc94d06700419123c6f99f0def40f4330
MarceloArraes <marcelo.arraes@gmail.com>
2022-09-14 11:52:45 +0000

    feat: aula 3 rocketseat added prisma, sqlite and more

--- server/build/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-    console.log('req', req);
-    res.send('Hello World');
-});
-app.listen(3000);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,33 @@
-const express = require('express');
+import express  from "express";
 
 const app = express();
 
-app.get('/', (req: any, res: { send: (arg0: string) => void; }) => {
-  console.log('req', req);
-  res.send('Hello World');
+app.get('/games/:id/ads', (req: any, res: any) => {
+  return res.json([
+    {id: 1, name: 'ad1'},
+    {id: 2, name: 'ad2'},
+    {id: 3, name: 'ad3'},
+    {id: 4, name: 'ad4'},
+    {id: 5, name: 'ad5'},
+    {id: 6, name: 'ad6'},
+  ]);
+});
+
+app.post('/ads', (req: any, res: any) => {
+  return res.status(201).json([]);
+});
+
+app.get('/ads/:id/discord', (req: any, res: any) => {
+
+  return res.status(200).json([
+    {id: 1, name: 'ad1'},
+    {id: 2, name: 'ad2'},
+    {id: 3, name: 'ad3'},
+    {id: 4, name: 'ad4'},
+    {id: 5, name: 'ad5'},
+    {id: 6, name: 'ad6'},
+  ]);
+
 });
 
 app.listen(3000)
