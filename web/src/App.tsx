0bc4e85a32aa55c57bdb0b7ce55cbe477fd9fc8e
MarceloArraes <marcelo.arraes@gmail.com>
2022-09-16 11:04:03 +0000

    feat: web finished for now

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './styles/main.css'
 import logoImg from './assets/logo-nlw-esports.png'
+import {MagnifyingGlassPlus} from 'phosphor-react'
 
 function App() {
   return (
-    <div className='max-w-[1344px] mx-auto flex items-center flex-col'>
+    <div className='max-w-[1344px] my-20 mx-auto flex items-center flex-col'>
       <img src={logoImg} alt="logo nlw sports" />
       <h1 className='text-6xl text-white font-black mt-20'>
         Seu <span className='bg-duo-gradient bg-clip-text text-transparent'>duo</span> está aqui.
@@ -56,12 +57,13 @@ function App() {
         </a>
       </div>
       <div className='pt-1 mt-8 bg-duo-gradient self-stretch rounded-lg overflow-hidden '>
-        <div className='bg-[#2A2634] px-8 py-6 rounded-lg self-stretch flex justify-between'>
+        <div className='bg-[#2A2634] px-8 py-6 items-center flex justify-between'>
           <div>
             <strong className='text-2xl text-white font-black'>Não encontrou seu duo?</strong>
             <span className='text-zinc-400 block '>Publique um anúncio para encontrar novos players!</span>
           </div>
-          <button className='py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600'>
+          <button className='py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24}/>
             Publicar anúncio
           </button>
         </div>
